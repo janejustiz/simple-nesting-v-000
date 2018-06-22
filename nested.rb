303cf8269d35e039ebe60646dd7d92332c7d2ffc
Jane Justiz <janejustiz@gmail.com>
2018-06-22 03:44:01 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -97,9 +97,8 @@ def changing_alan
           :languages => ["C"]
         }
      }
-
-		 programmer_hash[:alan_kay][:known_for] = alans_new_info
      alans_new_info = "GUI"
+		 programmer_hash[:alan_kay][:known_for] = alans_new_info
 end
 
 def adding_to_dennis
