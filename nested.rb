ae2b356965a48ae61705575283173ddb489f0910
Jane Justiz <janejustiz@gmail.com>
2018-06-22 03:38:03 +0000

    automatically backed up by learn

--- nested.rb
+++ nested.rb
@@ -98,6 +98,7 @@ def changing_alan
      }
      alans_new_info = "GUI"
 		 programmer_hash[:alan_kay][:known_for] = alans_new_info
+		 programmer_hash[:alan_kay][:known_for][1]
 end
 
 def adding_to_dennis
